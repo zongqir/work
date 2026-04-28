8fe319a1e4b8ac4c7ca27ec1de066115630faf30
zongqir <zongqir@qq.com>
2026-04-28 15:20:22 +0800

    2

--- code/aggregate_render_demo/messages/result.go
+++
@@ -1,8 +0,0 @@
-package main
-
-// AggregateResult 是上游返回结果。
-// 顶层固定有一个 message_type，再带一个具体消息体。
-type AggregateResult struct {
-	MessageType   string         `json:"message_type"`
-	XdrRiskDigest *XdrRiskDigest `json:"xdr_risk_digest,omitempty"`
-}
